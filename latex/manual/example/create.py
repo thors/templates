af6a9c0e837d7b98c7dc3d9d4bb9a2a7488ca19c
Thorsten Möllers <github@thorzten.de>
2013-09-18 14:37:37 +0200

    First version

---
+++ latex/manual/example/create.py
@@ -0,0 +1,28 @@
+import sys,os
+
+def system(command):
+    print(command)
+    os.system(command)
+
+def dot2png():
+    for (root, folders, files) in os.walk("."):
+        for f in files:
+            if f.lower().find(".dot") > 0:
+                ifn = root + os.sep + f;
+                ofn = ifn.lower().replace(".dot",".png");
+                system("dot -Tpng " + ifn + " > " + ofn);
+
+def latex(): 
+    system("latex main.tex")
+    system("makeindex main.idx")
+    system("latex main.tex")
+
+def clear():                
+    system("rm -f main.aux main.dvi main.idx main.ilg main.ind main.lof main.log main.lot main.toc")
+
+def main():                
+    clear()
+    dot2png()
+    latex()
+
+main()
