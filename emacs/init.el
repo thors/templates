433b00390e60fdce74a6e2ce0b4845dc91ba4331
Thorsten Möllers <github@thorzten.de>
2013-09-19 08:57:29 +0200

    Add more latex examples, fix template, add emacs example init.el file

---
+++ emacs/init.el
@@ -0,0 +1,16 @@
+(message (concat "loading " user-init-file))
+;;Disable auto-linebreak in AUCTeX
+(add-hook 'LaTeX-mode-hook 'turn-off-auto-fill) 
+(add-hook 'emacs-startup-hook 'delete-other-windows)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
